43b39da4ff298e5a85e7ca1e244512d07720ed90
Iván Fouilloux Alfaro <ivanfouilloux@MacBook-Pro-de-Ivan.local>
2020-07-17 21:47:43 -0500

    correcion setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from distutils.core import setup
 setup(
     name='covid_mx_data',
@@ -18,17 +19,10 @@ setup(
         'beautifulsoup4',
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 5 - Production/Stable',
-        # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
 )
